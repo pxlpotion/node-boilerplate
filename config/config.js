bf892a111e1b16ca14195ca563cbde962a2a35d1
Nathan Loyer <4namlet@gmail.com>
2016-09-26 09:17:32 -0500

    remove local env (dev is local), rename test to staging

--- config/config.js
+++ config/config.js
@@ -3,39 +3,32 @@
 const path = require('path');
 const rootPath = path.normalize(__dirname + '/..');
 const env = process.env.NODE_ENV || 'local';
+const appname = '6th Man Movers'
 
 const config = {
-  local: {  // DEFAULT : for running on local machine
-    env: 'local_dev',
-    root: rootPath,
-    app: {
-      name: 'app-name'
-    },
-    port: 3000
-  },
   development: {
     env: 'development',
     root: rootPath,
     app: {
-      name: 'app-name'
+      name: appname
     },
     port: 3000
   },
-  test: {
-    env: 'test',
+  staging: {
+    env: 'staging',
     root: rootPath,
     app: {
-      name: 'app-name'
+      name: appname
     },
-    port: 3000
+    port: process.env.PORT
   },
   production: {
     env: 'production',
     root: rootPath,
     app: {
-        name: 'app-name'
+        name: appname
     },
-    port: 3000
+    port: process.env.PORT
   }
 };
 
