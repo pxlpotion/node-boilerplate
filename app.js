07e9ac48da069018162cdf2a50c6d4a97ab444d9
DaddyWarbucks <beau@pxlpotion.com>
2017-01-04 23:57:43 -0600

    Move the contents of `config/express` directly into `app.js`

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
 'use strict';
 
 const express = require('express'),
-      config = require('./config/config'),
-      app = express();
+			app = express(),
+      fs = require('fs'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser'),
+      compress = require('compression'),
+      methodOverride = require('method-override'),
+      favicon = require('serve-favicon'),
+			config = require('./config/config');
 
-// Configure Express
-require('./config/express')(app, config);
+// Favicon & Static files
+// NOTE: This is where/how files are served from the diff build dirs for dev/prod
+let assetsPath = config.root + config.app.assets_path;
+app.use(express.static(assetsPath));
+app.use(favicon(assetsPath + '/images/favicon.ico'));
+
+// Views
+app.set('views', config.root + '/app/server/views');
+app.set('view engine', 'pug');
+
+// Body/Cookie Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// Make some stuff available to Pug
+app.locals.moment = require('moment');
+app.locals.env = config.env;
+
+// Manage Req's and Res's
+app.use(compress());
+app.use(methodOverride());
+
+// Recursive loading will load all files in given path and all subdirectories
+const loadRecursively = (path, app, config) => {
+	fs.readdirSync(path).forEach((file) => {
+		if (file.indexOf('.js') >= 0) {
+			require(path + '/' + file)(app, config);
+		} else {
+			loadRecursively(path + '/' + file, app, config);
+		}
+	});
+};
+
+// Load Middleware
+const middlewaresPath = config.root + '/app/server/middlewares';
+loadRecursively(middlewaresPath, app, config);
+
+// Load Controllers
+const controllersPath = config.root + '/app/server/controllers';
+loadRecursively(controllersPath, app, config);
 
 // Start the server
 const server = app.listen(config.port, () => {
-  console.log(`${config.app.name} listening on PORT : ${config.port}`);
+	console.log(`${config.app.name} listening on PORT : ${config.port}`);
 });
 
 // Exception handling
 process.on('uncaughtException', (err) => {
-  console.error(err);
-  server.close(() => {
-    process.exit(1);
-  });
+	console.error(err);
+	server.close(() => {
+		process.exit(1);
+	});
 });
+
